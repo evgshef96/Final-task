aaa37f978deb9c2baa9a1dd708874dade99b8811
Evgeny <jeka-291@mail.ru>
2022-09-20 22:40:53 +0300

    Создание исходного массива

---
+++ Final_task_code/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+string[] GetArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
